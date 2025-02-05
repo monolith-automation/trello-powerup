48d8077978271e4d64ce7c34845788717f72a43b
monolith-automation <Monolith-automation-team@monolith-law.jp>
2025-02-05 14:17:13 +0900

    Update powerup.js

---
+++ powerup.js
@@ -0,0 +1,21 @@
+window.TrelloPowerUp.initialize({
+    'card-buttons': function (t) {
+      return [{
+        text: '時間を記録',
+        callback: function (t) {
+          return t.modal({
+            url: './index.html',
+            height: 400,
+            fullscreen: false,
+            title: "稼働時間を記録しますか？"
+          });
+        }
+      }];
+    }
+  });
+  
+  function startTimer() {
+    console.log("✅ 記録開始！");
+    window.TrelloPowerUp.getRestApi().closeModal();
+  }
+  
